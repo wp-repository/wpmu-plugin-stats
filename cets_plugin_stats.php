de5e71ee61d157e381282edc8b27c32d50005f15
Christian Foellmann <foellmann@foe-services.de>
2013-12-19 12:44:25 +0100

    removed support for external deprecated plugins

--- cets_plugin_stats.php
+++ cets_plugin_stats.php
@@ -3,7 +3,7 @@
 Plugin Name: WPMU Plugin Stats
 Plugin URI: http://wordpress.org/plugins/wpmu-plugin-stats/
 Description: WordPress plugin for letting site admins easily see what plugins are actively used on which sites
-Version: 2.0-beta
+Version: 2.0
 Author: Kevin Graeme, <a href="http://deannaschneider.wordpress.com/" target="_target">Deanna Schneider</a> & <a href="http://www.jasonlemahieu.com/" target="_target">Jason Lemahieu</a>
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -56,7 +56,7 @@ class WPMU_Plugin_Stats {
 	 *
 	 * @var string
 	 */
-	public $version = '2.0-beta';
+	public $version = '2.0';
 
 	function __construct() {
 		
@@ -184,16 +184,16 @@ class WPMU_Plugin_Stats {
 		ksort( $list );
 
 		// if you're using plugin commander, these two values will be populated
-		$auto_activate = explode( ',', get_site_option( 'pc_auto_activate_list' ) );
-		$user_control = explode( ',', get_site_option( 'pc_user_control_list' ) );
+//		$auto_activate = explode( ',', get_site_option( 'pc_auto_activate_list' ) );
+//		$user_control = explode( ',', get_site_option( 'pc_user_control_list' ) );
 
 		// if you're using plugin manager, these values will be populated
-		$pm_auto_activate				= explode( ',', get_site_option( 'mp_pm_auto_activate_list' ) );
-		$pm_user_control				= explode( ',', get_site_option( 'mp_pm_user_control_list' ) );
-		$pm_supporter_control			= explode(',',get_site_option('mp_pm_supporter_control_list'));
-		$pm_auto_activate_status		= ( $pm_auto_activate[0] == '' || $pm_auto_active[0] == 'EMPTY' ? 0 : 1 );
-		$pm_user_control_status			= ( $pm_user_control[0] == '' || $pm_user_control == 'EMPTY' ? 0 : 1);
-		$pm_supporter_control_status	= ( $pm_supporter_control[0] == '' || $pm_supporter_control == 'EMPTY' ? 0 : 1 );
+//		$pm_auto_activate				= explode( ',', get_site_option( 'mp_pm_auto_activate_list' ) );
+//		$pm_user_control				= explode( ',', get_site_option( 'mp_pm_user_control_list' ) );
+//		$pm_supporter_control			= explode(',',get_site_option('mp_pm_supporter_control_list'));
+//		$pm_auto_activate_status		= ( $pm_auto_activate[0] == '' || $pm_auto_active[0] == 'EMPTY' ? 0 : 1 );
+//		$pm_user_control_status			= ( $pm_user_control[0] == '' || $pm_user_control == 'EMPTY' ? 0 : 1);
+//		$pm_supporter_control_status	= ( $pm_supporter_control[0] == '' || $pm_supporter_control == 'EMPTY' ? 0 : 1 );
 
 		// this is the built-in sitewide activation
 		$active_sitewide_plugins = maybe_unserialize( get_site_option( 'active_sitewide_plugins' ) );
@@ -205,18 +205,6 @@ class WPMU_Plugin_Stats {
 			.bloglist {
 				display:none;
 			}
-			.pc_settings_heading {
-				text-align: center; 
-				border-right: 3px solid black;
-				border-left: 3px solid black;
-
-			}
-			.pc_settings_left {
-				border-left: 3px solid black;
-			}
-			.pc_settings_right {
-				border-right: 3px solid black;
-			}
 			span.plugin-not-found {
 				color: red;
 			}
@@ -225,45 +213,13 @@ class WPMU_Plugin_Stats {
 			}
 		</style>
 		<div class="wrap">
-			<?php screen_icon( 'plugins' ); ?>
 			<h2><?php _e( 'Plugin Stats', 'wpmu-plugin-stats' ); ?></h2>
 			<table class="widefat" id="cets_active_plugins">
 				<thead>
-					<?php if ( sizeOf($auto_activate) > 1 || sizeOf($user_control) > 1 || $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) { ?>
 					<tr>
-						<th style="width: 25%;" >&nbsp;</th>
-						<?php if ( sizeOf( $auto_activate ) > 1 || sizeOf( $user_control ) > 1 ) { ?>
-							<th colspan="2" class="pc_settings_heading"><?php printf( '%s %s', __( 'Plugin Commander', 'wpmu-plugin-stats' ), __( 'Settings' ) ); ?></th>
-						<?php }
-						if ( $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) { ?>
-							<th colspan="3" align="center" class="pc_settings_heading">
-								<?php printf( '%s %s', __( 'Plugin Manager', 'wpmu-plugin-stats' ), __( 'Settings' ) ); ?>
-							</th>
-						<?php } ?>
-						<th>&nbsp;</th>
-						<th>&nbsp;</th>
-						<th style="width: 20%;">&nbsp;</th>
-					</tr>
-					<?php } ?>
-					<tr>
-						<th class="nocase"><?php _e( 'Plugin', 'wpmu-plugin-stats' ); ?></th>
-						<?php 
-						if ( sizeOf($auto_activate) > 1 || sizeOf($user_control) > 1 ) { ?>
-							<th class="nocase pc_settings_left">
-								<?php _e( 'Auto Activate', 'wpmu-plugin-stats' ); ?>
-							</th>
-							<th class="nocase pc_settings_right">
-								<?php _e( 'User Controlled', 'wpmu-plugin-stats' ); ?>
-							</th>
-							<?php	
-						}
-						if ( $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) {
-							?>
-							<th class="nocase pc_settings_left"><?php _e( 'Auto Activate', 'wpmu-plugin-stats' ); ?></th>
-							<th class="nocase"><?php _e( 'User Controlled', 'wpmu-plugin-stats' ); ?></th>
-							<th class="nocase pc_settings_right"><?php _e( 'Supporter Controlled', 'wpmu-plugin-stats' ); ?></th>
-							<?php	
-						} ?>
+						<th class="nocase">
+							<?php _e( 'Plugin', 'wpmu-plugin-stats' ); ?>
+						</th>
 						<th class="case" style="text-align: center !important">
 							<?php _e( 'Activated Sitewide', 'wpmu-plugin-stats' ); ?>
 						</th>
@@ -276,51 +232,10 @@ class WPMU_Plugin_Stats {
 					</tr>
 				</thead>
 				<tfoot>
-					<?php if ( sizeOf($auto_activate) > 1 || sizeOf($user_control) > 1 || $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) { ?>
 					<tr>
-						<th style="width: 25%;" >&nbsp;</th>
-						<?php if ( sizeOf( $auto_activate ) > 1 || sizeOf( $user_control ) > 1 ) { ?>
-							<th colspan="2" class="pc_settings_heading">
-								<?php printf( '%s %s', __( 'Plugin Commander', 'wpmu-plugin-stats' ), __( 'Settings')); ?>
-							</th>
-						<?php	
-						}
-						if ( $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) { ?>
-							<th colspan="3" align="center" class="pc_settings_heading">
-								<?php printf( '%s %s', __( 'Plugin Manager', 'wpmu-plugin-stats' ), __( 'Settings')); ?>
-							</th>
-						<?php	
-						}
-						?>
-						<th>&nbsp;</th>
-						<th>&nbsp;</th>
-						<th style="width: 20%;">&nbsp;</th>
-					</tr>
-					<?php }	?>
-					<tr>
-						<th class="nocase"><?php _e( 'Plugin', 'wpmu-plugin-stats' ); ?></th>
-						<?php if ( sizeOf( $auto_activate ) > 1 || sizeOf( $user_control ) > 1 ) { ?>
-							<th class="nocase pc_settings_left">
-								<?php _e( 'Auto Activate', 'wpmu-plugin-stats' ); ?>
-							</th>
-							<th class="nocase pc_settings_right">
-								<?php _e( 'User Controlled', 'wpmu-plugin-stats' ); ?>
-							</th>
-						<?php	
-						}
-						if ( $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) { ?>
-							<th class="nocase pc_settings_left">
-								<?php _e( 'Auto Activate', 'wpmu-plugin-stats' ); ?>
-							</th>
-							<th class="nocase">
-								<?php _e( 'User Controlled', 'wpmu-plugin-stats' ); ?>
-							</th>
-							<th class="nocase pc_settings_right">
-								<?php _e( 'Supporter Controlled', 'wpmu-plugin-stats' ); ?>
-							</th>
-						<?php	
-						}
-						?>
+						<th class="nocase">
+							<?php _e( 'Plugin', 'wpmu-plugin-stats' ); ?>
+						</th>
 						<th class="case" style="text-align: center !important">
 							<?php _e( 'Activated Sitewide', 'wpmu-plugin-stats' ); ?>
 						</th>
@@ -352,57 +267,12 @@ class WPMU_Plugin_Stats {
 						}
 
 						echo ( $thisName . '</td>' );
-						// plugin commander columns	
-						if ( sizeOf( $auto_activate ) > 1 || sizeOf($user_control) > 1 ) {
-							
-							echo '<td align="center" class="pc_settings_left">';
-							
-							if ( in_array( $file, $auto_activate ) ) { 
-								_e( 'Yes' );
-							} else {
-								_e( 'No');
-							}
-							
-							echo '</td><td align="center" class="pc_settings_right">';
-							
-							if ( in_array( $file, $user_control ) ) {
-								_e( 'Yes' );
-							} else {
-								_e( 'No' );
-							}
-							echo '</td>';
-						}
-						// plugin manager columns
-						if ( $pm_auto_activate_status == 1 || $pm_user_control_status == 1 || $pm_supporter_control_status == 1 ) {
-							echo ( '<td align="center" class="pc_settings_left">');
-							if (in_array($file, $pm_auto_activate)) {
-								_e( 'Yes' );
-							}
-							else {
-								_e( 'No' );
-							}
-							echo('</td><td align="center">');
-							if (in_array($file, $pm_user_control)) {
-								_e( 'Yes' );
-							}
-							else {
-								_e( 'No' );
-							}
-							echo '</td><td align="center" class="pc_settings_right">';
-							if ( in_array( $file, $pm_supporter_control ) ) {
-								_e( 'Yes' );
-							}
-							else {
-								_e( 'No' );
-							}
-							echo '</td>';
-						}
 
 						echo '<td align="center">';
+						
 						if ( is_array( $active_sitewide_plugins ) && array_key_exists( $file, $active_sitewide_plugins ) ) {
 							_e( 'Yes' );
-						}
-						else {
+						} else {
 							_e( 'No' );
 						}
 
@@ -414,11 +284,11 @@ class WPMU_Plugin_Stats {
 
 						echo '</td><td align="center">' . $numBlogs . '</td><td>';
 						?>
-						<a href="javascript:void(0)" onClick="jQuery('#bloglist_<?php echo $counter; ?>').toggle(400);"><?php _e( 'Show/Hide Blogs', 'wpmu-plugin-stats' ); ?></a>
-
+						<a href="javascript:void(0)" onClick="jQuery('#bloglist_<?php echo $counter; ?>').toggle(400);">
+							<?php _e( 'Show/Hide Blogs', 'wpmu-plugin-stats' ); ?>
+						</a>
+						<ul class="bloglist" id="bloglist_<?php echo $counter; ?>">
 						<?php
-						echo '<ul class="bloglist" id="bloglist_' . $counter  . '">';
-						
 						if ( isset( $info['blogs'] ) && is_array( $info['blogs'] ) ) {
 							foreach( $info['blogs'] as $blog ) {
 								echo '<li><a href="http://' . $blog['url'] . '" target="new">' . $blog['name'] . '</a></li>';
@@ -431,18 +301,24 @@ class WPMU_Plugin_Stats {
 					} ?>
 				</tbody>
 			</table>
-			<p>
-				<?php 
-					if ( time() - $gen_time > 60 ) { 
-						$lastregen = ( round( ( time() - $gen_time ) / 60, 0 ) ) . ' ' . __( 'minutes', 'wpmu-plugin-stats' );
-					} else { 
-						$lastregen = __( 'less than 1 minute', 'wpmu-plugin-stats' ); 
-					}
-					printf( __( 'This data is not updated as blog users update their plugins. It was last generated %s ago.', 'wpmu-plugin-stats' ), $lastregen ); ?> 
-					<form name="plugininfoform" action="" method="post">
-						<input type="submit" class="button-primary" value="<?php _e( 'Regenerate', 'wpmu-plugin-stats' ); ?>"><input type="hidden" name="action" value="update" />
-					</form>
-			</p>
+				
+			<?php
+			// @TODO nonce?
+			if ( time() - $gen_time > 60 ) { 
+				$lastregen = ( round( ( time() - $gen_time ) / 60, 0 ) ) . ' ' . __( 'minutes', 'wpmu-plugin-stats' );
+			} else { 
+				$lastregen = __( 'less than 1 minute', 'wpmu-plugin-stats' ); 
+			}
+			printf( __( 'This data is not updated as blog users update their plugins. It was last generated %s ago.', 'wpmu-plugin-stats' ), $lastregen );
+			?>
+			<div>
+				<form name="plugininfoform" action="" method="post">
+					<input type="submit" class="button-primary" value="<?php _e( 'Regenerate', 'wpmu-plugin-stats' ); ?>">
+					<input type="hidden" name="action" value="update" />
+				</form>
+			</div>
+		</div><!-- .wrap -->
+		
 	<?php
 	} // END plugin_stats_page()
 	
